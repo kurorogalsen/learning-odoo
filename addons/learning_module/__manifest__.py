069ea4c5c507f439ea52c5088c2da1e1ce9ea701
Moustapha Kachab <moustaphakachab@esp.sn>
2021-11-15 13:37:23 +0000

    Initialisation

---
+++ addons/learning_module/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Fake Learning',
+    'version': '1.0',
+    'category': 'Uncategorized',
+    'description': """
+A first odoo module I'm making for fun
+""",
+    'depends': ['base'],
+    'author': "Moustapha Kachab",
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'auto_install': True,
+}
